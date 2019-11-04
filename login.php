653719c356cab1bf7d1c870047812c815ed96b5b
phanthithuytrang <55264431+phanthithuytrang@users.noreply.github.com>
2019-11-04 22:09:29 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,170 @@
+<?php
+
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kiemtra";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+mysqli_set_charset($conn,"utf8");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  if(isset($_POST['submit-login'])){
+
+    $user = trim($_POST['user']);
+    $pass = trim($_POST['pass']);
+  
+    if($user == '' || $pass == ''){
+      $messageError = 'Please check again !';
+    }else{
+      $sql = "SELECT * FROM `users` WHERE user='".$user."' AND passwordd='".$pass."'";
+      $conn->query($sql);
+
+      $result = $conn->query($sql);
+
+      if(mysqli_num_rows($result) > 0){
+
+        foreach ($result as  $row) {
+          $_SESSION['user'] = $row['user'];
+          $_SESSION['id'] = $row['id'];
+          break;
+        }
+
+        // $_SESSION['user'] = $user;
+        header('Location: contacts.php');
+      }else{
+        $messageError = 'Please check again !';
+      }
+      
+      
+    }
+
+  }else if(isset($_POST['submit-logout'])){
+    unset($_SESSION['user']);
+    unset($_SESSION['id']);
+    $messageSuccess = 'Log out success !';
+  }
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+  if(isset($_SESSION['user'])){
+    header('Location: contacts.php');
+  }else{
+    // header('Location: login.php');
+  }
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">	
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <div class="container my-2">
+
+    <?php if(isset($messageError)) : ?>
+      <div class="alert alert-danger">
+        <?php echo $messageError; ?>
+      </div>
+    <?php endif ?>
+
+    <?php if(isset($messageSuccess)) : ?>
+      <div class="alert alert-success">
+        <?php echo $messageSuccess; ?>
+      </div>
+    <?php endif ?>
+
+    <div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form " action="login.php" method="post">
+					<span class="login100-form-logo">
+						<i class="zmdi zmdi-landscape"></i>
+					</span>
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						Log in
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="user" placeholder="Username">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+
+					<div class="contact100-form-checkbox">
+						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+						<label class="label-checkbox100" for="ckb1">
+							Remember me
+						</label>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button name="submit-login" class="login100-form-btn" type="submit">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-90">
+						<a class="txt1" href="#">
+							Forgot Password?
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+  </div>
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="js/main.js"></script>
+
+
+</body>
+</html>
